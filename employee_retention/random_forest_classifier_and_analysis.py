13f960536a5a84b9879f9cbf9ab60f2e20fbfe9b
Your Name <christian.william.hansen@gmail.com>
2017-09-23 20:37:18 -0700

    random forest analysis bug fixes

--- employee_retention/random_forest_classifier_and_analysis.py
+++ employee_retention/random_forest_classifier_and_analysis.py
@@ -1,19 +1,21 @@
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import precision_recall_curve, roc_curve
+import sklearn.metrics as metrics
 import matplotlib.pyplot as plt
+import numpy as np
 
 def rfc_model_analysis(model,X,Y):
     # Model Must be a Random Forest Classifier
     X_train,X_test,y_train,y_test = train_test_split(X,Y)
     model.fit(X_train,y_train)
-    predict = model.predict_proba(X_test)[:,1]
+    predict = model.predict_proba(X_test)[:,0]
     
     prec, rec, thresh1 = precision_recall_curve(y_test,predict)
     fpr,tpr, thresh2 = roc_curve(y_test,predict)
+    
     plt.figure(figsize=(10,5))
     plt.subplot(1, 2, 1)
     plt.plot(rec,prec)
-
     plt.xlabel('recall')
     plt.ylabel('precision')
     plt.subplot(1,2,2)
@@ -24,18 +26,16 @@ def rfc_model_analysis(model,X,Y):
     plt.show()
 
     # F1 = 2 * (prec * rec) / (prec + rec)
-    thresh1 = list(thresh1)
-    thresh1.append(1)
+    thresh2 = list(thresh2)
+#     thresh2.append(1)
     plt.subplot(1,2,1)
-    plt.plot(thresh1,tpr)
+    plt.plot(thresh2,tpr)
     plt.title('TPR Versus Threshold')
     plt.ylabel('tpr')
     plt.xlabel('Threshold')
-    plt.show()
-
     # F1 = 2 * (prec * rec) / (prec + rec)
-    thresh2 = list(thresh2)
-    thresh2.append(1)
+#   thresh2 = list(thresh2)
+    #thresh2.append(1)
     plt.subplot(1,2,2)
     plt.plot(thresh2,fpr)
     plt.title('FPR Versus Threshold')
@@ -57,7 +57,7 @@ def rfc_model_analysis(model,X,Y):
     plt.title('Feature Importance')
 
     print("At threshold = 0.5")
-    print metrics.classification_report(y_test,y_pred>0.5)
-    print 'accuracy: ',metrics.accuracy_score(y_test,y_pred>0.5)
+    print metrics.classification_report(y_test,predict>0.5)
+    print 'accuracy: ',metrics.accuracy_score(y_test,predict>0.5)
     
     return(model)
